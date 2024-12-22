26bace5604ad3e8c706eb7d73a89a5d9847ee284
szolotova <s.zolotova@artlogics.ru>
2024-12-22 17:03:04 +0300

    25-1-9 перестал добавляться проект и не раотает удаление

--- test/del_project.py
+++ test/del_project.py
@@ -10,21 +10,21 @@ from random import randrange
 def test_del_project(app):
     app.session.login("administrator", "root")
     app.project.open_project_page()
-    #old_projects = app.project.get_project_list()
+    old_projects = app.project.get_project_list()
 
-    # Проверяем, есть ли проекты в списке
-    #if len(old_projects) == 0:
+     #Проверяем, есть ли проекты в списке
+    if len(old_projects) == 0:
          #Создаем новый проект, если список пуст
-    #    app.project.create(Project(name="tpN-97-", description="tpD;gajdlgkr7-9-"))
+        app.project.create(Project(name="tpN-97-", description="tpD;gajdlgkr7-9-"))
     old_projects = app.project.get_project_list()  # Обновляем список проектов
-    #print("Old projects:", old_projects)  # Для отладки
+    print("Old projects:", old_projects)  # Для отладки
     index = random.randrange(len(old_projects))
-    #print('\n Before = ', app.project.count_projects())
+    print('\n Before = ', app.project.count_projects())
     app.project.delete_project(index)
-    #print('\n After = ', app.project.count_projects())
+    print('\n After = ', app.project.count_projects())
 
     # Проверяем, что количество проектов уменьшилось на 1
-    #assert len(old_projects) - 1 == app.project.count_projects()
+    assert len(old_projects) - 1 == app.project.count_projects()
 
     new_projects = app.project.get_project_list()
     old_projects[index:index + 1] = []  # Удаляем проект из старого списка
