20947102b79e4e7fd12ec578d5d7cfcc750d7a71
szolotova <s.zolotova@artlogics.ru>
2024-12-24 13:33:28 +0300

    26-1-12 настраиваем java,james, etc пишем тест signup и фикстуру james

--- test/conftest.py
+++ test/conftest.py
@@ -21,10 +21,10 @@ target = None
 def load_config(file):
     global target
     if target is None:
-        #config_file = os.path.join(os.path.dirname(os.path.abspath(file)), file)
-        config_file = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(file))), "target.json")
-        with (open(config_file) as f):
-            target = json.load(f)
+        config_file = os.path.join(os.path.dirname(os.path.abspath(file)), file)
+        #config_file = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(file))), "target.json")
+        #with (open(config_file) as f):
+        #    target = json.load(f)
     return target
 
 @pytest.fixture(scope="session")
