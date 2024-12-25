81a5ad3fabaedfd1388ee090f08616047e863341
szolotova <s.zolotova@artlogics.ru>
2024-12-25 11:13:34 +0300

    26-1-20 + Получение почты по протоколу POP3+SOAP +письма читаем

--- fixture/soap.py
+++ fixture/soap.py
@@ -7,7 +7,7 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-2.6.4/api/soap/mantisconnect.php?wsdl") #???
+        client = Client("http://localhost/mantisbt-2.26.4/api/soap/mantisconnect.wsdl") #???
         try:
             client.service.mc_login(username, password)
             return True
