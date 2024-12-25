ee2e20ae7045c7ae12acce883e2da3899f77b4c8
szolotova <s.zolotova@artlogics.ru>
2024-12-25 18:43:32 +0300

    26-1-25  + Получение почты по протоколу POP3+SOAP +письма читаем  Успешно!  +поправила тесты на создание/удаление с учетом config

--- fixture/soap.py
+++ fixture/soap.py
@@ -1,14 +1,15 @@
 from suds.client import Client
 from suds import WebFault
 
+from model.project import Project
+
+
 class SoapHelper:
     def __init__(self,app):
         self.app = app
 
     def can_login(self, username, password):
-        #client = Client("http://localhost/mantisbt-2.26.4/api/soap/mantisconnect.wsdl") #???
         client = Client("http://localhost/mantisbt-2.26.4/api/soap/mantisconnect.php?wsdl")
-
         try:
             client.service.mc_login(username, password)
             return True
@@ -16,7 +17,17 @@ class SoapHelper:
             return False
 
     def get_project_soap_list(self, username, password):
-        client = Client('http://localhost/mantisbt-2.26.4/api/soap/mantisconnect.wsdl')
-        project_list = client.service.mc_projects_get_user_accessible(username, password)
-        return project_list
+        client = Client('http://localhost/mantisbt-2.26.4/api/soap/mantisconnect.php?wsdl')
+        project_list = client.service.mc_projects_get_user_accessible(username, password) #что-то с проджект словом
+        return self.convert_list(project_list)
+
+
+
+    def convert_list(self, project_list):
+        def convert(x):
+            return Project(name=x.name, description=x.description)
+        result = [convert(z) for z in project_list]
+        return sorted(result, key=lambda project: project.name)
+
+
 
--- test/compare_list_soap.py
+++ test/compare_list_soap.py
@@ -1,23 +1,20 @@
 import pytest
-from model.project import Project
-from fixture.project import ProjectHelper
+from typing_extensions import Counter
 from data.project import testdata
+from model.project import Project
+
 
 @pytest.mark.parametrize("project", testdata)
-def test_compare_list_soap(app, project):
+def test_compare_list_soap(app, project, config):
+    username = config["web_admin"]['username']
+    password = config["web_admin"]['password']
+    app.session.login("administrator", "root")
     app.project.open_project_page()
-    old_projects = app.project.get_project_soap_list()
-    print('/n было = ', app.project.count_projects())
+    old_projects = app.soap.get_project_soap_list(username, password)
     app.project.create(project)
-    print('/n стало = ', app.project.count_projects())
-    new_projects = app.project.get_project_soap_list()
+    new_projects = app.soap.get_project_soap_list(username, password)
     assert len(old_projects) + 1 == app.project.count_projects()
     old_projects.append(project)
-    sort_old_list = sorted(old_projects, key=Project.name)  # Сортируем старый список
-    sort_new_list = sorted(new_projects, key=Project.name)  # Сортируем новый список
+    sort_old_list = sorted(old_projects, key=Project.name)
+    sort_new_list = sorted(new_projects, key=Project.name)
     assert sort_old_list == sort_new_list
-
-
-
-
-
